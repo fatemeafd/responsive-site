dde55d72e89162c19d39d05977d527e1f50398c4
Fatemeh Ahmadifard <f.ahmaifard@snapp.market>
2026-06-21 17:25:28 +0330

    chore: change the sourc

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
+import { readdirSync } from "node:fs";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
+
+const htmlFiles = readdirSync(resolve(__dirname, "src")).filter((file) =>
+  file.endsWith(".html"),
+);
 
 export default defineConfig({
   plugins: [tailwindcss()],
